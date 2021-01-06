ed31c30f4b91e5bfec9369b6a7092f1a3f8455a4
sergio perez <sergio-luis98@hotmail.com>
2021-01-06 10:38:47 -0500

    the last detail for the navbar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 const navSlide = () => {
   const menuBtn = document.querySelector('.menu-icon');
   const menu = document.querySelector('.menu-list');
+  const navLinks = document.querySelectorAll('.menu-list li');
 
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu-active')
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.3}s`
+      }
+    })
+
+    menuBtn.classList.toggle('toggle')
   })
 }
 
